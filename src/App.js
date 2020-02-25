1811889c260cc5ef2c30e1f2abaf9fffff02e5fc
Amala Mary Paul <amalamarypaul@gmail.com>
2020-02-25 18:32:49 +0530

    Initial commit - basic setup to scribble and download

--- src/App.js
+++ src/App.js
@@ -1,26 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState } from 'react';
+import SignatureCanvas from 'react-signature-canvas'
 
 function App() {
+  const [trimmedDataURL,setTrimmedDataURL] = useState('');
+  const [isEmptyMessage, setIsEmptyMessage] = useState(false);
+ let sigPad = {}
+
+ const clear = () => {
+   if (JSON.stringify(sigPad)!=='{}') {
+     sigPad.clear()
+   }
+   setTrimmedDataURL('');
+ }
+
+ const preview = () => {
+   if (sigPad.isEmpty()) {
+    setIsEmptyMessage(true);
+   } else {
+     setIsEmptyMessage(false);
+     setTrimmedDataURL(sigPad.getTrimmedCanvas().toDataURL('image/png'));
+   }
+
+ }
+ console.log(trimmedDataURL);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles.container}>
+      <div>
+        {
+          isEmptyMessage ?<p style={ styles.emptyMessage}> Please scribble something</p>:null
+        }
+     {
+       trimmedDataURL?
+       <img src={trimmedDataURL} style={ styles.signatureContainer}/>
+       :
+       <div>
+         <SignatureCanvas penColor='black'
+        canvasProps={{width: 500, height: 200, className: 'sigCanvas'}}
+        backgroundColor="rgba(0,0,0,.1)"
+        ref={(ref) => {
+          sigPad = ref }}
+      />
+       </div>
+
+     }
+      </div>
+      {
+        trimmedDataURL?
+          <a href={trimmedDataURL} title='download'>download</a>
+        :
+        <button onClick={preview}>
+               preview
+       </button>
+
+      }
+      <button onClick={clear}>
+             clear
+     </button>
+
     </div>
+
   );
+}
+const styles = {
+  container:{
+    display:'flex',
+    flexDirection:'column',
+    alignItems:'center',
+    marginTop:50
+  },
+  signatureContainer:{
+    width:500,
+    height:200,
+    marginLeft:50
+  },
+  emptyMessage:{
+    color:'red',
+    fontSize:18,
+    textAlign:'center'
+  }
 }
 
 export default App;
