5a2e1b8864dd072af28c2aa25e144b6851bc1e3a
Amala Mary Paul <amalamarypaul@gmail.com>
2020-02-25 19:21:38 +0530

    WIP- working progress change pen color and background color

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, {useState } from 'react';
-import SignatureCanvas from 'react-signature-canvas'
+import SignatureCanvas from 'react-signature-canvas';
+import { SketchPicker } from 'react-color';
 
 function App() {
   const [trimmedDataURL,setTrimmedDataURL] = useState('');
   const [isEmptyMessage, setIsEmptyMessage] = useState(false);
+  const [signColor, setSignColor]=useState('black');
+  const [showPicker, setPickerVisibility]=useState(false);
+  const [colorType, setColorType] = useState('pen');
+  const [canvasColor, setCanvasColor] = useState('white');
  let sigPad = {}
 
  const clear = () => {
-   if (JSON.stringify(sigPad)!=='{}') {
+   if (Object.keys(sigPad).length===0) {
+     sigPad={}
+   }else{
      sigPad.clear()
    }
    setTrimmedDataURL('');
@@ -22,8 +29,18 @@ function App() {
    }
 
  }
- console.log(trimmedDataURL);
-
+ const handleChangeComplete = (color) => {
+   if (colorType==='pen') {
+     setSignColor(color.hex);
+   } else {
+     setCanvasColor(color.hex)
+   }
+setPickerVisibility(false);
+ };
+const showColorPicker = ({ type })=>{
+  setPickerVisibility(true);
+  setColorType(type)
+}
   return (
     <div style={styles.container}>
       <div>
@@ -35,9 +52,9 @@ function App() {
        <img src={trimmedDataURL} style={ styles.signatureContainer}/>
        :
        <div>
-         <SignatureCanvas penColor='black'
+         <SignatureCanvas penColor={signColor}
         canvasProps={{width: 500, height: 200, className: 'sigCanvas'}}
-        backgroundColor="rgba(0,0,0,.1)"
+        backgroundColor={canvasColor}
         ref={(ref) => {
           sigPad = ref }}
       />
@@ -57,6 +74,21 @@ function App() {
       <button onClick={clear}>
              clear
      </button>
+     <button onClick={()=>{showColorPicker({type:'pen'})}}>
+          Change pen color
+    </button>
+    <button onClick={()=>{showColorPicker({type:'canvas'})}}>
+         Change background color
+   </button>
+   {
+     showPicker?
+     <SketchPicker
+         color={ signColor }
+         onChangeComplete={ handleChangeComplete }
+       />:null
+
+   }
+
 
     </div>
 
@@ -67,12 +99,14 @@ const styles = {
     display:'flex',
     flexDirection:'column',
     alignItems:'center',
-    marginTop:50
+    marginTop:50,
+    backgroundColor:'rgba(0,0,0,.1)',
+    height:670
   },
   signatureContainer:{
     width:500,
     height:200,
-    marginLeft:50
+    //marginLeft:50
   },
   emptyMessage:{
     color:'red',
